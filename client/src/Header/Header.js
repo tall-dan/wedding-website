0b1e76d550e035b7e8190e4dee448b1f82949eb5
Dan Schepers <dan.schepers@yello.co>
2019-07-12 15:01:19 -0500

    stylelint compliance

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -3,7 +3,6 @@ import styles from './Header.module.scss';
 
 const Header = () => (
   <header className={styles.Header}>
-    <span>{styles.Header}</span>
     <img
       src="https://static.xoedge.com/xo-guest-services/assets/guest/themes/at_last-dark_gray/theme_image-84680f8ae087b2b676c7d5120efda905.png"
       alt="our-wedding"
