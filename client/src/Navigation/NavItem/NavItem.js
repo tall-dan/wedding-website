70b4df14bca0c36ab8063a57192d3df19a9f015d
Dan Schepers <dan.schepers@yello.co>
2019-08-04 16:01:20 -0500

    Fix active page for base route

--- client/src/Navigation/NavItem/NavItem.js
+++ client/src/Navigation/NavItem/NavItem.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { camelCase } from 'lodash';
 import styles from './NavItem.module.scss';
 
-const NavItem = ({ title }) => (
-  <div className={styles.NavItem}>
-    <NavLink
-      to={`/${camelCase(title)}`}
-      className={styles.NavItem__Link}
-      activeClassName={styles['NavItem__Link--selected']}
-    >
-      {title}
-    </NavLink>
-  </div>
-);
+class NavItem extends Component {
+  isActive = (_match, location) => {
+    if (location.pathname === '/') return this.props.title === 'Our Wedding';
+    return location.pathname === `/${camelCase(this.props.title)}`;
+  };
+
+  render() {
+    return (
+      <div className={styles.NavItem}>
+        <NavLink
+          to={`/${camelCase(this.props.title)}`}
+          isActive={this.isActive}
+          className={styles.NavItem__Link}
+          activeClassName={styles['NavItem__Link--selected']}
+        >
+          {this.props.title}
+        </NavLink>
+      </div>
+    );
+  }
+}
 
 NavItem.propTypes = {
   title: PropTypes.string.isRequired
