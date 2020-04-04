9ec29fd7fe2c1c6e983a0a39f022441b734fab7e
Dan Schepers <dan.schepers@yello.co>
2020-04-04 15:36:29 -0500

    update wedding date

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
 import moment from 'moment-timezone';
 
-export const ceremonyStart = moment.tz('2020-06-13 15:00', 'America/Chicago');
-export const ceremonyEnd = moment.tz('2020-06-13 15:30', 'America/Chicago');
-export const receptionStart = moment.tz('2020-06-13 17:00', 'America/Chicago');
-export const receptionEnd = moment.tz('2020-06-13 23:30', 'America/Chicago');
+export const ceremonyStart = moment.tz('2020-08-21 16:30', 'America/Chicago');
+export const ceremonyEnd = moment.tz('2020-08-21 17:15', 'America/Chicago');
+export const receptionStart = moment.tz('2020-08-21 17:30', 'America/Chicago');
+export const receptionEnd = moment.tz('2020-08-21 23:30', 'America/Chicago');
