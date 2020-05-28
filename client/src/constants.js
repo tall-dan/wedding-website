c69dcbf87b6b59881fcd5cb9840daf83a14ef986
Dan Schepers <dan.schepers@yello.co>
2020-05-27 21:56:44 -0500

    covid message

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,9 @@
 import moment from 'moment-timezone';
 
-export const ceremonyStart = moment.tz('2020-08-21 16:30', 'America/Chicago');
-export const ceremonyEnd = moment.tz('2020-08-21 17:15', 'America/Chicago');
-export const receptionStart = moment.tz('2020-08-21 17:30', 'America/Chicago');
-export const receptionEnd = moment.tz('2020-08-21 23:30', 'America/Chicago');
+export const ceremonyStart = moment.tz('2020-06-13 15:00', 'America/Chicago');
+export const ceremonyEnd = moment.tz('2022-06-13 15:45', 'America/Chicago');
+
+export const newCeremonyStart = moment.tz('2022-06-25 15:00', 'America/Chicago');
+export const newCeremonyEnd = moment.tz('2022-06-25 15:45', 'America/Chicago');
+export const newReceptionStart = moment.tz('2022-06-25 17:30', 'America/Chicago');
+export const newReceptionEnd = moment.tz('2022-06-25 23:30', 'America/Chicago');
