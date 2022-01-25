087474485b6d53b71d8af6c9152f7cbd62aa1a3f
Dan Schepers <daniel.schepers@rallyhealth.com>
2022-01-25 08:54:18 -0600

    update to new date. time unknown, afaik

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
 import moment from 'moment-timezone';
 
-export const ceremonyStart = moment.tz('2020-08-21 16:30', 'America/Chicago');
-export const ceremonyEnd = moment.tz('2020-08-21 17:15', 'America/Chicago');
-export const receptionStart = moment.tz('2020-08-21 17:30', 'America/Chicago');
-export const receptionEnd = moment.tz('2020-08-21 23:30', 'America/Chicago');
+export const ceremonyStart = moment.tz('2022-06-25 16:30', 'America/Chicago');
+export const ceremonyEnd = moment.tz('2022-06-25 17:15', 'America/Chicago');
+export const receptionStart = moment.tz('2022-06-25 17:30', 'America/Chicago');
+export const receptionEnd = moment.tz('2022-06-25 23:30', 'America/Chicago');
