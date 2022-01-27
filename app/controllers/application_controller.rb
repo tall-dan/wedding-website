f1c45b3f04114cb75f189dcb4d6a1089cc8f6c8b
Dan Schepers <daniel.schepers@rallyhealth.com>
2022-01-27 16:14:22 -0600

    add pulse check and assets for graphiql

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def pulse_check
+    head :ok
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   post '/graphql', to: 'graphql#execute'
+  get '/pulse_check', to: 'application#pulse_check'
   devise_for :users
 
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
