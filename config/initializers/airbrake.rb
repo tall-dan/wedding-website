8ff4027b87597c36798b67061e360fa16ae1e2cf
Dan Schepers <daniel.schepers@rallyhealth.com>
2022-03-31 15:23:05 -0500

    errbit

---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+Airbrake.configure do |config|
+  config.host = 'http://errors.mcschepers-wedding.com'
+  config.project_id = 1 # required, but any positive integer works
+  config.project_key = Rails.application.credentials.airbrake[:project_key]
+
+  # airbrake.io supports various features that are out of scope for
+  # Errbit. Disable them:
+  config.job_stats           = false
+  config.query_stats         = false
+  config.performance_stats   = false
+  config.remote_config       = false
+
+  config.environment = Rails.env
+end
