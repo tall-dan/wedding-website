6daf6cc7b1e58b520b077d81e359102e35d05cff
Dan Schepers <daniel.schepers@rallyhealth.com>
2022-04-07 20:21:57 -0500

    change after party end

--- client/src/constants.js
+++ client/src/constants.js
@@ -5,4 +5,4 @@ export const ceremonyEnd = moment.tz('2022-06-25 15:45', 'America/Chicago');
 export const receptionStart = moment.tz('2022-06-25 17:00', 'America/Chicago');
 export const receptionEnd = moment.tz('2022-06-25 23:00', 'America/Chicago');
 export const afterPartyStart = moment.tz('2022-06-25 23:00', 'America/Chicago');
-export const afterPartyEnd = moment.tz('2022-06-26 02:00', 'America/Chicago');
+export const afterPartyEnd = moment.tz('2022-06-26 01:00', 'America/Chicago');
