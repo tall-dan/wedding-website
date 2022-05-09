9b248e47345226317e6748932baf29b68fe67dc2
Dan Schepers <daniel.schepers@rallyhealth.com>
2022-05-09 16:28:28 -0500

    keep plugging

--- lib/google_sheet_reader.rb
+++ lib/google_sheet_reader.rb
@@ -26,6 +26,12 @@ class GoogleSheetReader
     sheets_service
   end
 
+  def creds_from_env
+    return nil unless ENV['GOOGLE_CREDENTIALS']
+
+    StringIO.new(ENV['GOOGLE_CREDENTIALS'])
+  end
+
   ##
   # Ensure valid credentials, either by restoring from the saved credentials
   # files or intitiating an OAuth2 authorization. If authorization is required,
@@ -34,7 +40,7 @@ class GoogleSheetReader
   # @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
   def authorize
     Google::Auth::ServiceAccountCredentials.make_creds(
-      json_key_io: ENV['GOOGLE_CREDENTIALS'] || File.open(CREDENTIALS_PATH),
+      json_key_io: creds_from_env || File.open(CREDENTIALS_PATH),
       scope: SCOPE
     ).tap(&:fetch_access_token!)
   end
